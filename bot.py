d30af1854b79175cc91a869511eec0d06d0a6132
Daniel Esteban Bejarano <danielebejaranor@gmail.com>
2024-11-08 20:39:23 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,31 +1,21 @@
-#Configuración del bot de discord
 import discord
-from Clase3_bot_logic import gen_pass, gen_emodji, flip_coin
-# La variable intents almacena los privilegios del bot
+from discord.ext import commands
+
 intents = discord.Intents.default()
-# Activar el privilegio de lectura de mensajes
 intents.message_content = True
-# Crear un bot en la variable cliente y transferirle los privilegios
-client = discord.Client(intents=intents)
 
-@client.event
+bot = commands.Bot(command_prefix='$', intents=intents)
+
+@bot.event
 async def on_ready():
-    print(f'Hemos iniciado sesión como {client.user}')
+    print(f'We have logged in as {bot.user}')
 
-@client.event
-async def on_message(message):
-    #Pude corregir el error de que el bot repitiera el mensaje
-    if message.author == client.user:
-        return
-    if message.content.startswith('$hola'):
-        await message.channel.send("Hola, Daniel")
-    elif message.content.startswith('$adiós'):
-        await message.channel.send("Hasta luego\U0001f642")
-    elif message.content.startswith("password"):
-        await message.channel.send(gen_pass(10))
-    elif message.content.startswith("emoji"):
-        await message.channel.send(gen_emodji())
-    elif message.content.startswith("moneda"):
-        await message.channel.send(flip_coin())
+@bot.command()
+async def hello(ctx):
+    await ctx.send(f'Hola, soy un bot {bot.user}!')
 
-client.run("MTMwMjA3NDk5NTUxMzYyNjY0NA.Go_RA3.BuWsviIBfnFOqIF-hDvXl22UdP5R2ZDGrfTfZk")
+@bot.command()
+async def heh(ctx, count_heh = 5):
+    await ctx.send("he" * count_heh)
+
+bot.run("MTMwMjA3NDk5NTUxMzYyNjY0NA.Go_RA3.BuWsviIBfnFOqIF-hDvXl22UdP5R2ZDGrfTfZk")
