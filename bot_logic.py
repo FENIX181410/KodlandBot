870e74e25e1e7163ca83687817af9f553dbe47a3
Daniel Esteban Bejarano <danielebejaranor@gmail.com>
2024-11-12 21:38:24 -0500

    Update bot_logic.py

--- bot_logic.py
+++ bot_logic.py
@@ -1,4 +1,3 @@
-#Funciones para el bot de discord
 import random
 
 def gen_pass(pass_length):
@@ -10,7 +9,7 @@ def gen_pass(pass_length):
 
     return password
 
-def gen_emodji():
+def gen_emoji():
     emodji = ["\U0001f600", "\U0001f642", "\U0001F606", "\U0001F923"]
     return random.choice(emodji)
 
@@ -20,3 +19,52 @@ def flip_coin():
         return "Cara"
     else:
         return "Cruz"
+    
+def juego():
+    print("Ok, cada uno empieza con una bala. Dime 'terminar' cuando quieras acabar el juego.")
+    balas = 1
+    opciones = ["recargar", "disparar", "bloquear"]
+    
+    while True:
+        jugador = input("Elige una opción (disparar, recargar, bloquear): ")
+        if jugador == "terminar":
+            print("El juego ha terminado.")
+            exit()
+        
+        if balas == 0:
+            opciones = ["recargar", "bloquear"] 
+        else:
+            opciones = ["recargar", "disparar", "bloquear"]
+        
+        elección = random.choice(opciones)
+        
+        if jugador == "disparar":
+            if elección == "disparar":
+                balas-=1
+                print("Los dos hemos disparado, las balas se chocan. Seguimos jugando.")
+            elif elección == "bloquear":
+                print("Me disparaste mientras bloqueaba. Seguimos jugando.")
+            elif elección == "recargar":
+                print("Me disparaste mientras recargaba. Has ganado.")
+                exit()
+ 
+        
+        elif jugador == "bloquear":
+            if elección == "disparar":
+                balas-=1
+                print("Te disparé mientras bloqueabas. Seguimos jugando.")
+            elif elección == "bloquear":
+                print("Los dos hemos bloqueado. Seguimos jugando.")
+            elif elección == "recargar":
+                balas+=1
+                print("He recargado mientras bloqueabas. Seguimos jugando.")
+        
+        elif jugador == "recargar":
+            if elección == "disparar":
+                print("Te he disparado mientras recargabas. He ganado.")
+                exit()
+            elif elección == "bloquear":
+                print("He bloqueado mientras recargaba. Seguimos jugando.")
+            elif elección == "recargar":
+                balas+=1
+                print("Los dos hemos recargado. Seguimos jugando.")
