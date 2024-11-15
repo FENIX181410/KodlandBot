6182c05655cb523bb746bf82201b0b75291a52f2
Daniel Esteban Bejarano <danielebejaranor@gmail.com>
2024-11-14 22:07:04 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,6 +1,6 @@
 import discord
 from discord.ext import commands
-from bot_logic import gen_pass, gen_emoji, flip_coin, juego
+from bot_logic2 import gen_pass, gen_emoji, flip_coin, juego
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -16,7 +16,7 @@ async def hola(ctx):
     await ctx.send(f'Hola, {ctx.author.name} soy el bot {bot.user}!')
 
 @bot.command()
-async def contraseña(ctx, x):
+async def contraseña(ctx, x:int):
     await ctx.send(gen_pass(x))
 
 @bot.command()
@@ -27,8 +27,31 @@ async def emoji(ctx):
 async def moneda(ctx):
     await ctx.send(flip_coin())
     
-@bot.command()
-async def juego(ctx):
-    await ctx.send(juego())
+juegos_activos = {}
 
-bot.run("TOKEN AQUÍ")
+@bot.command(name='jugar')
+async def jugar(ctx):
+    juegos_activos[ctx.author.id] = 1 
+    await ctx.send("¡Juego iniciado! Cada uno empieza con una bala. Escribe 'recargar', 'disparar' o 'bloquear'.")
+    
+@bot.command(name='accion')
+async def accion(ctx, eleccion: str):
+    if ctx.author.id not in juegos_activos:
+        await ctx.send("Primero debes iniciar el juego con el comando 'K!jugar'.")
+        return
+    
+    resultado = juego(eleccion.lower())
+    await ctx.send(resultado)
+    
+    if "ganado" in resultado or "terminado" in resultado:
+        juegos_activos.pop(ctx.author.id)
+        
+@bot.command(name='terminar')
+async def terminar(ctx):
+    if ctx.author.id in juegos_activos:
+        juegos_activos.pop(ctx.author.id)
+        await ctx.send("El juego ha terminado.")
+    else:
+        await ctx.send("No tienes un juego en curso.")
+
+bot.run("TOKEN")
